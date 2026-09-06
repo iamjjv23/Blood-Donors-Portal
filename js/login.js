d818a73bc77d1272cb2261c823ea34b8b7e52c7b
JJV <12014550+iamjjv23@users.noreply.github.com>
2026-09-06 08:45:32 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,13 @@
+(function() {
+        const currentUrl = window.location.href;
+        if (currentUrl.includes('.html')) 
+        {
+            const cleanUrl = currentUrl.replace(/\.html/g, '');
+            window.history.replaceState(null, null, cleanUrl);
+        }
+    })();
+
+
 const API = 'UPH';
         
   
